af660ab195c9bfd41ca32981c8f4689b1fc7a8ec
mrGuLoN <89205606+mrGuLoN@users.noreply.github.com>
2023-10-19 02:08:00 +0700

    Add 2d Generator

--- Assets/Underground Laboratory Generator/Scripts/LaboratoryGenerator.cs
+++ Assets/Underground Laboratory Generator/Scripts/LaboratoryGenerator.cs
@@ -1,109 +1,118 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using Random = UnityEngine.Random;
 
-public class LaboratoryGenerator : MonoBehaviour
+namespace Underground_Laboratory_Generator.Scripts
 {
-    public UnityEvent Done;
-    public bool GenerateOnStart = true;
-    [Range(3, 100)]
-    public int RoomCount = 9;
-    public LayerMask CellLayer;
-
-    public GameObject InsteadDoor;
-    public GameObject[] DoorPrefabs;
-    public Cell[] CellPrefabs;
-    public static LaboratoryGenerator Instance;
-
-   public  List<LightProbeGroup> _lightProbeGroups = new();
-    void Awake() 
+    public class LaboratoryGenerator : MonoBehaviour
     {
-        Instance = this;
-    }
-    private void Start()
-    {
-        if (GenerateOnStart) StartCoroutine(StartGeneration());
-    }
+        public UnityEvent done;
+        [SerializeField] private bool generateOnStart = true;
+        [Range(2, 100)]
+        [SerializeField] private int roomCount = 9;
+        [SerializeField] private LayerMask cellLayer;
 
-    IEnumerator StartGeneration()
-    {
-        List<Transform> CreatedExits = new List<Transform>();
-        Cell StartRoom = Instantiate(CellPrefabs[Random.Range(0, CellPrefabs.Length)], Vector3.zero, Quaternion.identity);
-        _lightProbeGroups.Add(StartRoom.LightProbeGroup);
-        for (int i = 0; i < StartRoom.Exits.Length; i++) CreatedExits.Add(StartRoom.Exits[i].transform);
-        StartRoom.TriggerBox.enabled = true;
+        [SerializeField] private GameObject insteadDoor;
+        [SerializeField] private GameObject[] doorPrefabs;
+        [SerializeField] private Cell[] cellPrefabs;
+        public static LaboratoryGenerator Instance;
 
-        int limit = 1000, roomsLeft = RoomCount - 1;
-        while (limit > 0 && roomsLeft > 0)
+   
+        void Awake() 
         {
-            limit--;
+            Instance = this;
+        }
+        private void Start()
+        {
+            if (generateOnStart) StartCoroutine(StartGeneration());
+        }
 
-            Cell selectedPrefab = Instantiate(CellPrefabs[Random.Range(0, CellPrefabs.Length)], Vector3.zero, Quaternion.identity);
-          
-            int lim = 100;
-            bool collided;
-            Transform selectedExit;
-            Transform createdExit; // из списка созданных входов
+        IEnumerator StartGeneration()
+        {
+            List<Transform> createdExits = new List<Transform>();
+            Cell startRoom = Instantiate(cellPrefabs[Random.Range(0, cellPrefabs.Length)], Vector3.zero, Quaternion.identity) ?? throw new ArgumentNullException(nameof(Instantiate) + "(cellPrefabs[Random.Range(0, cellPrefabs.Length)], Vector3.zero, Quaternion.identity)");
+       
+            for (int i = 0; i < startRoom.Exits.Length; i++) createdExits.Add(startRoom.Exits[i].transform);
+            startRoom.TriggerBox.enabled = true;
 
-            selectedPrefab.TriggerBox.enabled = false; // чтобы сам себя не проверял на наличие нахлеста ВЫКЛЮЧИЛ
-
-            do
+            int limit = 1000, roomsLeft = roomCount - 1;
+            while (limit > 0 && roomsLeft > 0)
             {
-                lim--;
+                limit--;
 
-                createdExit = CreatedExits[Random.Range(0, CreatedExits.Count)];
-                selectedExit = selectedPrefab.Exits[Random.Range(0, selectedPrefab.Exits.Length)].transform;
+                Cell selectedPrefab = Instantiate(cellPrefabs[Random.Range(0, cellPrefabs.Length)], Vector3.zero, Quaternion.identity);
+          
+                int lim = 100;
+                bool collided;
+                Transform selectedExit;
+                Transform createdExit; // из списка созданных входов
 
-                // rotation
-                float shiftAngle = createdExit.eulerAngles.z + 180 - selectedExit.eulerAngles.z;
-                selectedPrefab.transform.Rotate(new Vector3(0, shiftAngle, 0)); // выходы повернуты друг напротив друга
+                selectedPrefab.TriggerBox.enabled = false; // чтобы сам себя не проверял на наличие нахлеста ВЫКЛЮЧИЛ
 
-                // position
-                Vector3 shiftPosition = createdExit.position - selectedExit.position;
-                selectedPrefab.transform.position += shiftPosition; // выходы состыковались
+                do
+                {
+                    lim--;
 
-                // check
-                Vector3 center = selectedPrefab.transform.position + selectedPrefab.TriggerBox.center.z * selectedPrefab.transform.forward
-                    + selectedPrefab.TriggerBox.center.y * selectedPrefab.transform.up
-                    + selectedPrefab.TriggerBox.center.x * selectedPrefab.transform.right; // selectedPrefab.TriggerBox.center
-                Vector3 size = selectedPrefab.TriggerBox.size / 2f; // half size
-                Quaternion rot = selectedPrefab.transform.localRotation;
-                collided = Physics.CheckBox(center, size, rot, CellLayer, QueryTriggerInteraction.Collide);
+                    createdExit = createdExits[Random.Range(0, createdExits.Count)];
+                    selectedExit = selectedPrefab.Exits[Random.Range(0, selectedPrefab.Exits.Length)].transform;
+
+                    // rotation
+                    float shiftAngle = createdExit.eulerAngles.z + 180 - selectedExit.eulerAngles.z;
+                    Transform selectedPrefabTransform;
+                    (selectedPrefabTransform = selectedPrefab.transform).Rotate(new Vector3(0, 0, shiftAngle)); // выходы повернуты друг напротив друга
+
+                    // position
+                    Vector3 shiftPosition = createdExit.position - selectedExit.position;
+                    var position = selectedPrefabTransform.position;
+                    position += shiftPosition; // выходы состыковались
+                    selectedPrefabTransform.position = position;
+
+                    // check
+                    var triggerBoxCenter = selectedPrefab.TriggerBox.center;
+                    Vector3 center = position + triggerBoxCenter.z * selectedPrefabTransform.forward
+                                              + triggerBoxCenter.y * selectedPrefabTransform.up
+                                              + triggerBoxCenter.x * selectedPrefabTransform.right; // selectedPrefab.TriggerBox.center
+                    Vector3 size = selectedPrefab.TriggerBox.size / 2f; // half size
+                    Quaternion rot = selectedPrefabTransform.localRotation;
+                    collided = Physics.CheckBox(center, size, rot, cellLayer, QueryTriggerInteraction.Collide);
+
+                    yield return new WaitForEndOfFrame();
+
+                } while (collided && lim > 0);
+
+                selectedPrefab.TriggerBox.enabled = true; // ВКЛЮЧИЛ
+
+                if (lim > 0)
+                {
+                    roomsLeft--;
+
+                    for (int j = 0; j < selectedPrefab.Exits.Length; j++) createdExits.Add(selectedPrefab.Exits[j].transform);
+
+                    createdExits.Remove(createdExit);
+                    createdExits.Remove(selectedExit);
+
+                    Transform exitTransform;
+                    Instantiate(doorPrefabs[Random.Range(0, doorPrefabs.Length)], (exitTransform = createdExit.transform).position, exitTransform.rotation);
+                    DestroyImmediate(createdExit.gameObject);
+                    DestroyImmediate(selectedExit.gameObject);
+                }
+                else DestroyImmediate(selectedPrefab.gameObject);
 
                 yield return new WaitForEndOfFrame();
-
-            } while (collided && lim > 0);
-
-            selectedPrefab.TriggerBox.enabled = true; 
-            _lightProbeGroups.Add(selectedPrefab.LightProbeGroup);// ВКЛЮЧИЛ
-
-            if (lim > 0)
-            {
-                roomsLeft--;
-
-                for (int j = 0; j < selectedPrefab.Exits.Length; j++) CreatedExits.Add(selectedPrefab.Exits[j].transform);
-
-                CreatedExits.Remove(createdExit);
-                CreatedExits.Remove(selectedExit);
-
-                Instantiate(DoorPrefabs[Random.Range(0, DoorPrefabs.Length)], createdExit.transform.position, createdExit.transform.rotation);
-                DestroyImmediate(createdExit.gameObject);
-                DestroyImmediate(selectedExit.gameObject);
             }
-            else DestroyImmediate(selectedPrefab.gameObject);
 
-            yield return new WaitForEndOfFrame();
+            // instead doors
+            for (int i = 0; i < createdExits.Count; i++)
+            {
+                Instantiate(insteadDoor, createdExits[i].position, createdExits[i].rotation);
+                DestroyImmediate(createdExits[i].gameObject);
+            }
+
+            Debug.Log("Finished " + Time.time);
+            done.Invoke();
         }
-
-        // instead doors
-        for (int i = 0; i < CreatedExits.Count; i++)
-        {
-            Instantiate(InsteadDoor, CreatedExits[i].position, CreatedExits[i].rotation);
-            DestroyImmediate(CreatedExits[i].gameObject);
-        }
-
-        Debug.Log("Finished " + Time.time);
-        Done.Invoke();
     }
 }
